cbe0d670befc0a0a14f4a78f86e6959763165aae
Thiago Tomei Fernandez <thiago.tomei@cern.ch>
2021-06-23 16:09:10 -0300

    Dynamically created Task and Sequence

--- TrackingResolution/python/RClusterSeq_cff.py
+++ TrackingResolution/python/RClusterSeq_cff.py
@@ -4,6 +4,10 @@ from TrackingResolution.TrackingResolution.RClusterProducer_cfi import (
     rCluster as rCluster,
 )
 
+RClusterTask = cms.Task()
+
+RClusterTask.add(rCluster)
+
 goodMuons = cms.EDFilter(
     "GoodRecoMuonsFilter",
     trackslabel=cms.InputTag("generalTracks"),
@@ -13,17 +17,11 @@ goodMuons = cms.EDFilter(
     filter=cms.bool(True),
 )
 
+RClusterTask.add(goodMuons)
+
 for suf in range(3, 9):
     moduleLabel = "rCluster" + str(suf)
     vars()[moduleLabel] = rCluster.clone(layersRemaining=cms.uint32(suf))
+    RClusterTask.add(vars()[moduleLabel])
 
-RClusterSeq = cms.Sequence(
-    goodMuons
-    + rCluster
-    + rCluster8
-    + rCluster7
-    + rCluster6
-    + rCluster5
-    + rCluster4
-    + rCluster3
-)
+RClusterSeq = cms.Sequence(RClusterTask)
